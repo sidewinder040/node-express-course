9b1ff53084ad8ebd9ff6445fea7c96a3ab417c29
sidewinder040 <macevic040@hotmail.co.uk>
2021-01-29 23:13:47 +0000

    3) Added Stock route

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+const stockAvailability=[
+    {
+        part: '47000',
+        stock: 300
+    },
+    {
+        part: '23405',
+        stock: 340
+    }
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
+app.get('/stock', function(req, res) {
+    res.json({
+        success: true,
+        message: "Stock Availability",
+        stock: stockAvailability
+    })
+})
+
+app.listen(8000,function(){console.log('server is listening')})
